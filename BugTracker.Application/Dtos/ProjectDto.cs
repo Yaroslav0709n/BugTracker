d3357e91176bd2f15622f9832b37b42c08a7d751
Yaroslav0709 <vizichkanich0709@gmail.com>
2023-05-04 15:48:35 +0300

    add dtos

---
+++ BugTracker.Application/Dtos/ProjectDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Application.Dtos
+{
+    public class ProjectDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+}

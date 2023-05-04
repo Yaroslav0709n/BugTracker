e7b94bb89797689507b0bbb1cfa6b48020e2947c
Yaroslav0709 <vizichkanich0709@gmail.com>
2023-05-04 15:49:15 +0300

    add projectservices

---
+++ BugTracker.Application/IServices/IProjectService.cs
@@ -0,0 +1,14 @@
+﻿using BugTracker.Application.Dtos;
+using BugTracker.Domain.Entities;
+
+namespace BugTracker.Application.IServices
+{
+    public interface IProjectService
+    {
+        Task<IEnumerable<Project>> GetAllProjects();
+        Task<Project> GetById(int id);
+        Task<Project> CreateProject(string userId, ProjectDto project);
+        Task UpdateProject(ProjectDto project);
+        Task DeleteProject(int id);
+    }
+}

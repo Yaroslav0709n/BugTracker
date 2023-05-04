41dc5624f4e48fd93c9d131e5f7fbde365129457
Yaroslav0709 <vizichkanich0709@gmail.com>
2023-05-04 15:47:55 +0300

    add mapper

---
+++ BugTracker.Application/Mappings/ProjectMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BugTracker.Application.Dtos;
+using BugTracker.Domain.Entities;
+
+
+namespace BugTracker.Application.Mappings
+{
+    public class ProjectMapper:Profile
+    {
+        public ProjectMapper()
+        {
+            CreateMap<ProjectDto, Project>();
+        }
+    }
+}

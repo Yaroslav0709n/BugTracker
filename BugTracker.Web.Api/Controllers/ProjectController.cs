1695dbd5fdd6a1b6bf1bd071f85980537f6edfa6
Yaroslav0709 <vizichkanich0709@gmail.com>
2023-05-04 15:43:12 +0300

    add project controller

---
+++ BugTracker.Web.Api/Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using BugTracker.Application.Dtos;
+using BugTracker.Application.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpPost]
+        public async Task<ProjectDto> AddProject(string userId, ProjectDto projectDto)
+        {
+            await _projectService.CreateProject(userId, projectDto);
+            return projectDto;
+        }
+    }
+}

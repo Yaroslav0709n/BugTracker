e8b0df9b52b0d1dc9fd8d66a0587a3fc9fd8e365
Yaroslav0709 <vizichkanich0709@gmail.com>
2023-05-04 15:45:56 +0300

    add new repository

---
+++ BugTracker.Infrastructure/Repositories/ProjectRepository.cs
@@ -0,0 +1,42 @@
+﻿using BugTracker.Domain.Entities;
+using BugTracker.Domain.IRepositories;
+using BugTracker.Infrastructure.Context.ApplicationDbContext;
+
+namespace BugTracker.Infrastructure.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly ApplicationContext _context;
+        public ProjectRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Project> AddProject(Project project)
+        {
+            await _context.Project.AddAsync(project);
+            await _context.SaveChangesAsync();
+            return project;
+        }
+
+        public Task DeleteProject(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Project>> GetAllProjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Project> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProject(Project project)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

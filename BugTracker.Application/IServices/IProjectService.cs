f168b3e5dbd58b3ed563fb3fb35988b02a0a73e1
Ярослав <vizichkanich0709@gmail.com>
2023-05-15 22:56:03 +0300

    add/refactor iservice

--- BugTracker.Application/IServices/IProjectService.cs
+++ BugTracker.Application/IServices/IProjectService.cs
@@ -1,14 +1,14 @@
-﻿using BugTracker.Application.Dtos;
+﻿using BugTracker.Application.Dtos.Project;
 using BugTracker.Domain.Entities;
 
 namespace BugTracker.Application.IServices
 {
     public interface IProjectService
     {
-        Task<IEnumerable<Project>> GetAllProjects();
-        Task<Project> GetById(int id);
-        Task<Project> CreateProject(string userId, ProjectDto project);
-        Task UpdateProject(ProjectDto project);
-        Task DeleteProject(int id);
+        Task<IEnumerable<ProjectDto>> GetAllProjects(string userId);
+        Task<ProjectDto> GetProject(int projectId);
+        Task<Project> CreateProject(ProjectDto projectDto, string userId);
+        Task<UpdateProjectDto> UpdateProject(int projectId, UpdateProjectDto projectDto);
+        Task DeleteProject(int projectId);
     }
 }
---
+++ BugTracker.Application/IServices/IProjectUserService.cs
@@ -0,0 +1,12 @@
+﻿using BugTracker.Application.Dtos.Project;
+
+namespace BugTracker.Application.IServices
+{
+    public interface IProjectUserService
+    {
+        Task AddUserProject(string userId, int projectId);
+        Task<IEnumerable<UsersProjectsDto>> GetAllProjectsUser(int projectId);
+        Task<IEnumerable<UsersProjectsDto>> GetNonProjectUsers(int projectId);
+        Task RemoveFromProject(string userId, int projectId);
+    }
+}

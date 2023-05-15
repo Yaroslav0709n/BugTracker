796ed87b1b234cdfad296c5334a22ae2312780df
Ярослав <vizichkanich0709@gmail.com>
2023-05-15 23:04:36 +0300

    add repo

---
+++ BugTracker.Domain/IRepositories/IProjectUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using BugTracker.Domain.Entities;
+using BugTracker.Domain.Entities.Identity;
+
+namespace BugTracker.Domain.IRepositories
+{
+    public interface IProjectUserRepository
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllProjectsUserAsync(int projectId);
+        Task<IEnumerable<ApplicationUser>> GetNonProjectsUserAsync(int projectId);
+        Task<ProjectUser> CreateUserProjectAsync(ProjectUser userProject);
+        Task RemoveFromProjectAsync(string userId, int projectId);
+    }
+}

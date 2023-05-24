1c324d2742f80f90800e8cd7f0d5e9329f6c0ee5
Ярослав <vizichkanich0709@gmail.com>
2023-05-24 14:44:06 +0300

    add ticket user service

---
+++ BugTracker.Application/IServices/ITicketUserService.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.Application.Dtos.Ticket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.IServices
+{
+    public interface ITicketUserService
+    {
+        Task<IEnumerable<UsersTicketsDto>> GetAllTicketUsers(int ticketId);
+        Task<IEnumerable<UsersTicketsDto>> GetNonTicketUsers(int ticketId);
+        Task AddUserTicket(string userId, int ticketId);
+        Task RemoveFromTicket(string userId, int ticketId);
+    }
+}

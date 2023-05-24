9d26e81caa32e34582e85776980421a97a718f11
Ярослав <vizichkanich0709@gmail.com>
2023-05-24 15:06:14 +0300

    create validation

---
+++ BugTracker.Application/Validation/ProjectValidator.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Application.Dtos.Project;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Validation
+{
+    public class ProjectValidator : AbstractValidator<ProjectDto>
+    {
+        public ProjectValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(40)
+                .WithMessage("'Name' must be no longer than 40 characters");
+            RuleFor(x => x.Description).NotEmpty().MaximumLength(250)
+                .WithMessage("'Description' must be no longer than 250 characters");
+        }
+    }
+}
---
+++ BugTracker.Application/Validation/RegisterUserValidator.cs
@@ -0,0 +1,22 @@
+﻿using BugTracker.Application.Dtos.User;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Validation
+{
+    public class RegisterUserValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterUserValidator() 
+        {
+            RuleFor(register => register.Email).EmailAddress();
+            RuleFor(register => register.FirstName).NotEmpty().MinimumLength(1).MaximumLength(250);
+            RuleFor(register => register.LastName).NotEmpty().MinimumLength(1).MaximumLength(250);
+            RuleFor(register => register.Password).NotEmpty().MinimumLength(6)
+                .WithMessage("'Password' must be at least 6 characters long");
+        }
+    }
+}

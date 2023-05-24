59dcb5a93e925438aba57070cd791ec5e92b0ede
Ярослав <vizichkanich0709@gmail.com>
2023-05-24 14:44:27 +0300

    create ticket mapper

---
+++ BugTracker.Application/Mappings/TicketMapper.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BugTracker.Application.Dtos.Ticket;
+using BugTracker.Domain.Entities;
+using BugTracker.Domain.Entities.Identity;
+
+namespace BugTracker.Application.Mappings
+{
+    public class TicketMapper:Profile
+    {
+        public TicketMapper()
+        {
+            CreateMap<AddTicketDto, Ticket>();
+            CreateMap<Ticket, AddTicketDto>();
+
+            CreateMap<TicketDto, Ticket>();
+            CreateMap<Ticket, TicketDto>();
+
+            CreateMap<InfoAboutTicketDto, Ticket>();
+            CreateMap<Ticket, InfoAboutTicketDto>();
+
+            CreateMap<UpdateTicketDto, Ticket>();
+            CreateMap<Ticket, UpdateTicketDto>();
+
+            CreateMap<ApplicationUser, UsersTicketsDto>();
+            CreateMap<UsersTicketsDto, ApplicationUser>();
+        }
+    }
+}

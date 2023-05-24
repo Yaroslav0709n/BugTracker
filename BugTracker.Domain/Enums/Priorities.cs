df6d9046ec6de3b124129b6e7a668761ec33cbb5
Ярослав <vizichkanich0709@gmail.com>
2023-05-24 15:08:06 +0300

    create enums

---
+++ BugTracker.Domain/Enums/Priorities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Domain.Enums
+{
+    public enum Priorities
+    {
+        Low,
+        Medium,
+        High
+    }
+}
---
+++ BugTracker.Domain/Enums/Types.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Domain.Enums
+{
+    public enum Types
+    {
+        Bug,
+        Feature,
+        Enhancement,
+        Task
+    }
+}

d5c8e3210f980203793b3c9490e9857f80076944
Ярослав <vizichkanich0709@gmail.com>
2023-05-24 15:08:49 +0300

    create repo for ticket and ticketuser

---
+++ BugTracker.Domain/IRepositories/ITicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Domain.IRepositories
+{
+    public interface ITicketRepository
+    {
+        Task<IEnumerable<Ticket>> GetAllTicketsAsync(int projectId);
+        Task<Ticket> GetTicketAsync(int ticketId);
+        Task<Ticket> AddTicketAsync(Ticket ticket, string userId, int projectId);
+        Task<Ticket> UpdateTicketAsync(Ticket ticket);
+        Task DeleteTicketAsync(int ticketId);
+    }
+}
---
+++ BugTracker.Domain/IRepositories/ITicketUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Domain.Entities;
+using BugTracker.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Domain.IRepositories
+{
+    public interface ITicketUserRepository
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllTicketsUserAsync(int ticketId);
+        Task<IEnumerable<ApplicationUser>> GetNonTicketsUserAsync(int ticketId);
+        Task<TicketUser> CreateUserTicketAsync(TicketUser userTicket);
+        Task RemoveFromTicketAsync(string userId, int ticketId);
+    }
+}

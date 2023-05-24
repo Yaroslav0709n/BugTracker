0fcb3fdf62525fd0f5e429770846ba9eae66f45d
Ярослав <vizichkanich0709@gmail.com>
2023-05-24 15:12:21 +0300

    extensions for validation

---
+++ BugTracker.Web.Api/Extensions/ValidationServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BugTracker.Application.Dtos.Project;
+using BugTracker.Application.Dtos.User;
+using BugTracker.Application.Validation;
+using FluentValidation;
+
+namespace BugTracker.Web.Api.Extensions
+{
+    public static class ValidationServicesExtensions
+    {
+        public static IServiceCollection AddValidationService(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<RegisterDto>, RegisterUserValidator>();
+            services.AddTransient<IValidator<ProjectDto>, ProjectValidator>();
+
+            return services;
+        }
+    }
+}

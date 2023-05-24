8f0839db06a963f1d70071f61ff45d806e5ca0b5
Ярослав <vizichkanich0709@gmail.com>
2023-05-24 14:43:32 +0300

    refactor code

--- BugTracker.Application/IServices/IProjectUserService.cs
+++ BugTracker.Application/IServices/IProjectUserService.cs
@@ -4,9 +4,9 @@ namespace BugTracker.Application.IServices
 {
     public interface IProjectUserService
     {
-        Task AddUserProject(string userId, int projectId);
-        Task<IEnumerable<UsersProjectsDto>> GetAllProjectsUser(int projectId);
+        Task<IEnumerable<UsersProjectsDto>> GetAllProjectUsers(int projectId);
         Task<IEnumerable<UsersProjectsDto>> GetNonProjectUsers(int projectId);
+        Task AddUserProject(string userId, int projectId);
         Task RemoveFromProject(string userId, int projectId);
     }
 }

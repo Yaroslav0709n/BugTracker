6d0b378b7c5cbe7c953a2354812431fb5f48ac70
Ярослав <vizichkanich0709@gmail.com>
2023-05-28 14:19:09 +0300

    create controller

---
+++ BugTracker.Web.Api/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using BugTracker.Application.Dtos.User;
+using BugTracker.Application.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetUserById(string userId) 
+        {
+            var user = await _userService.GetUser(userId);
+            return Ok(user);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUserById([FromBody] UpdateUserDto updateUserDto, string userId)
+        {
+            var updateUser = await _userService.UpdateUser(updateUserDto, userId);
+            return Ok(updateUserDto);
+        }
+    }
+}

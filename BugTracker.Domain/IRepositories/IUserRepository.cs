269c68bef4d68cc9afa56ff2ee1f76c1f8d1d792
Ярослав <vizichkanich0709@gmail.com>
2023-05-28 14:17:52 +0300

    create irepository

---
+++ BugTracker.Domain/IRepositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker.Domain.Entities.Identity;
+
+namespace BugTracker.Domain.IRepositories
+{
+    public interface IUserRepository
+    {
+        Task<ApplicationUser> GetUserAsync(string userId);
+        Task<ApplicationUser> UpdateUserDataAsync(ApplicationUser user);
+    }
+}

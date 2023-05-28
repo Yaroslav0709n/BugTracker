653c7abbae650703724f10379a3bf947a364d750
Ярослав <vizichkanich0709@gmail.com>
2023-05-28 14:15:39 +0300

    create null validation

---
+++ BugTracker.Application/ValidationExtensions/NullValidationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.ValidationExtensions
+{
+    public static class NullValidationExtensions
+    {
+        public static T ThrowIfNull<T>(this T argument, string argumentName)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+
+            return argument;
+        }
+    }
+}

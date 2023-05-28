959e7957cf376360153c4b76e30931c62edb726d
Ярослав <vizichkanich0709@gmail.com>
2023-05-28 14:08:08 +0300

    add new prop

--- BugTracker.Application/Dtos/Ticket/TicketDto.cs
+++ BugTracker.Application/Dtos/Ticket/TicketDto.cs
@@ -1,16 +1,10 @@
-﻿using BugTracker.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BugTracker.Application.Dtos.Ticket
+﻿namespace BugTracker.Application.Dtos.Ticket
 {
     public class TicketDto
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public string CreatorName { get; set; }
+        public string CreatedByUserId { get; set; }
+        public string CreatedUserName { get; set; }
     }
 }

32947378bb8426bf09936eea3f32fb5a84cda001
Ярослав <vizichkanich0709@gmail.com>
2023-05-28 14:12:36 +0300

    mapper for users

---
+++ BugTracker.Application/Mappings/UserMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BugTracker.Application.Dtos.User;
+using BugTracker.Domain.Entities.Identity;
+
+namespace BugTracker.Application.Mappings
+{
+    public class UserMapper:Profile
+    {
+        public UserMapper()
+        {
+            CreateMap<UserDto, ApplicationUser>();
+            CreateMap<ApplicationUser, UserDto>();
+
+            CreateMap<UpdateUserDto, ApplicationUser>();
+            CreateMap<ApplicationUser, UpdateUserDto>();
+        }
+    }
+}

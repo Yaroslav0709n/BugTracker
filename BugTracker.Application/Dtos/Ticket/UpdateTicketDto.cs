6391386b1db8416f05775f0ff63c296548385051
Ярослав <vizichkanich0709@gmail.com>
2023-05-28 14:14:40 +0300

    initial commit

--- BugTracker.Application/Dtos/Ticket/UpdateTicketDto.cs
+++ BugTracker.Application/Dtos/Ticket/UpdateTicketDto.cs
@@ -1,9 +1,4 @@
 ﻿using BugTracker.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BugTracker.Application.Dtos.Ticket
 {

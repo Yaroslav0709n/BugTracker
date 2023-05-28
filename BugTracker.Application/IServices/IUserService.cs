70caa77e0e680abf5dc356b6367243f176e73b7b
Ярослав <vizichkanich0709@gmail.com>
2023-05-28 14:10:51 +0300

    new iservice

---
+++ BugTracker.Application/IServices/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using BugTracker.Application.Dtos.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.IServices
+{
+    public interface IUserService
+    {
+        Task<UserDto> GetUser(string userId);
+        Task<UpdateUserDto> UpdateUser(UpdateUserDto updateUserDto, string userId);
+    }
+}

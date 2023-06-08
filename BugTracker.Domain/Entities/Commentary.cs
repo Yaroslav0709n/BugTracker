ae0dc9255b125dd730add2826df45591ae0b95f5
Ярослав <vizichkanich0709@gmail.com>
2023-06-08 20:52:57 +0300

    new entities

---
+++ BugTracker.Domain/Entities/Commentary.cs
@@ -0,0 +1,19 @@
+﻿using BugTracker.Domain.Entities.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Domain.Entities
+{
+    public class Commentary
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public int TicketId { get; set; }
+        [ForeignKey("TicketId")]
+        public Ticket Ticket { get; set; }
+        public string CreatedByUserId { get; set; }
+        [ForeignKey("CreatedByUserId")]
+        public ApplicationUser CreatedByUser { get; set; }
+    }
+}

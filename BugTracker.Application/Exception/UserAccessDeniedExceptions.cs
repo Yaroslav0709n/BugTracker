50fc7042cb0225d5be667915ca0390ab4dba699e
Ярослав <vizichkanich0709@gmail.com>
2023-06-08 20:48:48 +0300

    initial commit

---
+++ BugTracker.Application/Exception/UserAccessDeniedExceptions.cs
@@ -0,0 +1,7 @@
+﻿namespace BugTracker.Application.Exception
+{
+    public class UserAccessDeniedExceptions : System.Exception
+    {
+        public UserAccessDeniedExceptions(string name) : base($"User: {name} access denied!") { }
+    }
+}
---
+++ BugTracker.Application/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BugTracker.Application.Exception;
+using System.Security.Claims;
+
+namespace BugTracker.Application.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetCurrentUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentNullException(nameof(principal));
+            }
+            var result = Guid.TryParse(
+                principal.Claims
+                    .FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier
+                )?.Value, out var userId);
+            if (!result)
+            {
+                throw new UserAccessDeniedExceptions(principal.ToString());
+            }
+            return userId;
+        }
+    }
+}

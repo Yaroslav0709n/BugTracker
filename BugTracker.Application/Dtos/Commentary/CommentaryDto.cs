a061e607b24c6830905a5dff7b1acf5ba6e754eb
Ярослав <vizichkanich0709@gmail.com>
2023-06-08 20:43:46 +0300

    initial commit

---
+++ BugTracker.Application/Dtos/Commentary/CommentaryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BugTracker.Application.Dtos.Commentary
+{
+    public class CommentaryDto
+    {
+        public string Text { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string CreatedByUserId { get; set; }
+    }
+}

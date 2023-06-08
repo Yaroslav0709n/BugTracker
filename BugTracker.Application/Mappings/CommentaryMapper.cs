c36d299ada579a3535939208ce422a635f725fb0
Ярослав <vizichkanich0709@gmail.com>
2023-06-08 20:49:49 +0300

    new mapper

---
+++ BugTracker.Application/Mappings/CommentaryMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BugTracker.Application.Dtos.Commentary;
+using BugTracker.Domain.Entities;
+
+namespace BugTracker.Application.Mappings
+{
+    internal class CommentaryMapper:Profile
+    {
+        public CommentaryMapper()
+        {
+            CreateMap<AddCommentaryDto, Commentary>();
+            CreateMap<Commentary, AddCommentaryDto>();
+
+            CreateMap<CommentaryDto, Commentary>();
+            CreateMap<Commentary, CommentaryDto>();
+        }
+    }
+}

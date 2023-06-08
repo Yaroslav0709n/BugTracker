f682c7479a608c180946fbb1df0a4285de49343a
Ярослав <vizichkanich0709@gmail.com>
2023-06-08 20:51:41 +0300

    new validator

---
+++ BugTracker.Application/Validation/AddCommentaryValidator.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.Application.Dtos.Commentary;
+using FluentValidation;
+
+namespace BugTracker.Application.Validation
+{
+    public class AddCommentaryValidator:AbstractValidator<AddCommentaryDto>
+    {
+        public AddCommentaryValidator()
+        {
+            RuleFor(x => x.Text)
+                .NotEmpty()
+                .WithMessage("'Text' must be filled.")
+                .MaximumLength(1000)
+                .WithMessage("'Text' must be no longer than 1000 characters");
+        }
+    }
+}

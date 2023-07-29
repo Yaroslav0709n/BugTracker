c54514415068e957bebc7d8e6082f4b2ac39f693
Ярослав <vizichkanich0709@gmail.com>
2023-07-29 11:57:37 +0300

    handle auth errors

---
+++ BugTracker.Application/Common/Exceptions/ErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace BugTracker.Application.Common.Exceptions
+{
+    public class ErrorResponse
+    {
+        public string Error { get; set; }
+
+        //public ErrorResponse(string error)
+        //{
+        //    Error = error;
+        //}
+    }
+}

9bc235269310b658441126476f6dcf12d40623ca
Ярослав <vizichkanich0709@gmail.com>
2023-08-03 00:07:41 +0300

    middlewares

---
+++ BugTracker.Web.Api/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Text.Json;
+using BugTracker.Application.Common.Exceptions;
+using FluentValidation;
+
+namespace BugTracker.Web.Api.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger,
+            IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(Exception exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    code = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Errors);
+                    break;
+                case NotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+
+            }
+            context.Response.ContentType = "apptlication/json";
+            context.Response.StatusCode = (int)code;
+
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { errpr = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
+
+
+
+
+
+
---
+++ BugTracker.Web.Api/Middlewares/ExceptionHandlerMiddlewareExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Web.Api.Middlewares
+{
+    public static class ExceptionHandlerMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}

142b8b031c2dab78c0fe2d27396888d5c41af01f
Ярослав <vizichkanich0709@gmail.com>
2023-08-03 08:11:19 +0300

    initial commit

--- BugTracker.Web.Api/Controllers/AuthController.cs
+++ BugTracker.Web.Api/Controllers/AuthController.cs
@@ -4,7 +4,6 @@ using BugTracker.Application.IServices;
 using BugTracker.Domain.Entities.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Octokit.Internal;
 
 namespace BugTracker.Web.Api.Controllers
 {
